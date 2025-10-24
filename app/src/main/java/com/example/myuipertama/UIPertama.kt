bb3a2b3ab284853b228a2066e435849785c7d5fb
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-10-24 10:05:29 +0700

    mengatur stayl untuk card nya

--- app/src/main/java/com/example/myuipertama/UIPertama.kt
+++ app/src/main/java/com/example/myuipertama/UIPertama.kt
@@ -43,7 +43,12 @@ fun ActivitasPertama(modifier: Modifier){
             fontSize = 22.sp
         )
         Spacer(modifier = Modifier.height(height = 25.dp))
-        Card ()
+        Card (
+            modifier = Modifier
+                .fillMaxWidth(fraction = 1f)
+                .padding(all = 12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.DarkGray
+            )
 
-}
 
