020b1f2550ec2f9ce63fe0d5b9dc0758e5e667dd
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-10-24 09:34:53 +0700

    Menambahkan komponen Image untuk menampilkan logo aplikasi di dalam Row

--- app/src/main/java/com/example/myuipertama/UIPertama.kt
+++ app/src/main/java/com/example/myuipertama/UIPertama.kt
@@ -44,7 +44,16 @@ fun ActivitasPertama(modifier: Modifier){
             colors = CardDefaults.cardColors(
                 containerColor = Color.DarkGray
             )
-
+        ) {
+            Row() {
+                val gambar = painterResource(id = R.drawable.logo)
+                Image(
+                   painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(size = 100.dp)
+                )
+            }
         }
     }
 }
