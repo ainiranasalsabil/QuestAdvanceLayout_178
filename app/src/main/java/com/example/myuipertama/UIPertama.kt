a4428eb21955c126f4fea9e8a8453f52b63951a9
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-10-24 10:07:29 +0700

    Mengatur besar dari gambarnya

--- app/src/main/java/com/example/myuipertama/UIPertama.kt
+++ app/src/main/java/com/example/myuipertama/UIPertama.kt
@@ -54,3 +54,9 @@ fun ActivitasPertama(modifier: Modifier){
             Row() {
                 val gambar = painterResource(id = R.drawable.logo)
                 Image(
+                   painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(size = 100.dp)
+                        .padding(all = 5.dp)
+
